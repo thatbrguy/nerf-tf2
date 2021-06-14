74399ca2b322f953099b1d1d50536aabd0dfcfcb
thatbrguy <bharathrajn98@gmail.com>
2021-06-14 22:53:36 +0530

    add pose and plot utils

---
+++ nerf/utils/plot_utils.py
@@ -0,0 +1,100 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from mpl_toolkits import mplot3d
+from nerf.utils import pose_utils as PoseUtils
+
+def create_eye_frustum_for_plots(length, width, height):
+    """
+    TODO: Docstring
+    """
+
+    L, W, H = length, width, height
+
+    O = np.array([0., 0., 0.], dtype = np.float64)
+    A = np.array([-L/2, -W/2, H], dtype = np.float64)
+    B = np.array([-L/2,  W/2, H], dtype = np.float64)
+    C = np.array([ L/2,  W/2, H], dtype = np.float64)
+    D = np.array([ L/2, -W/2, H], dtype = np.float64)
+
+    lines = np.array([
+        [O, A], [O, B], [O, C], [O, D],
+        [A, B], [B, C], [C, D], [D, A],
+    ])
+
+    return lines
+
+def create_xyz_axes_for_plots(length):
+    """
+    TODO: Docstring
+    """
+    L = length
+
+    O = np.array([0., 0., 0.], dtype = np.float64)
+    X = np.array([  L,  0.,  0.], dtype = np.float64)
+    Y = np.array([ 0.,   L,  0.], dtype = np.float64)
+    Z = np.array([ 0.,  0.,   L], dtype = np.float64)
+
+    lines = np.array([
+        [O, X], [O, Y], [O, Z],
+    ])
+
+    return lines
+
+def plot_scene(c2w_matrices, plot_eye_frustums = True, plot_cam_axes = True):
+    """
+    Returns a 3D plot of the camera poses and other details.
+
+    TODO: Make length, width, height tunable!
+    """
+    
+    # Creating lines that constitute an eye frustum.
+    eye_frustum = create_eye_frustum_for_plots(0.4, 0.4, 0.6) 
+
+    # Creating lines that constitute an XYZ axes.
+    camera_axes = create_xyz_axes_for_plots(0.3)
+
+    # Moving the lines that constitute the eye frustum for each 
+    # camera to the world frame.
+    eyes_world_frame = PoseUtils.batched_transform_line_segments(
+        c2w_matrices, eye_frustum
+    )
+
+    # Moving the lines that constitute the XYZ axes for each 
+    # camera to the world frame.
+    cam_axes_world_frame = PoseUtils.batched_transform_line_segments(
+        c2w_matrices, camera_axes
+    )
+
+    ## TODO: Keep using ortho?
+    ax = plt.axes(projection = '3d', proj_type = 'ortho')
+
+    for eye, axes in zip(eyes_world_frame, cam_axes_world_frame):
+
+        # Plotting the lines that constitute the eye frustum.
+        if plot_eye_frustums:
+            for line in eye:
+                ax.plot3D(line[:, 0], line[:, 1], line[:, 2], color = 'red')
+
+        if plot_cam_axes:
+            # Plotting the XYZ axes.
+            ax.plot3D(axes[0, :, 0], axes[0, :, 1], axes[0, :, 2], color = 'red')
+            ax.plot3D(axes[1, :, 0], axes[1, :, 1], axes[1, :, 2], color = 'green')
+            ax.plot3D(axes[2, :, 0], axes[2, :, 1], axes[2, :, 2], color = 'blue')
+            ax.scatter3D(axes[0,0,0], axes[0,0,1], axes[0,0,2], s = 1.0, c = "black")
+
+    plt.show()
+
+if __name__ == "__main__":
+
+    np.set_printoptions(precision = 4, suppress = True)
+    path = "/path/to/poses_bounds.npy" ## placeholder path for now
+    arr = np.load(path)
+
+    poses = arr[:, :15].reshape(-1, 3, 5)[:, :3, :4]
+    last_row = np.tile([0, 0, 0, 1], (poses.shape[0], 1, 1))
+
+    c2w_matrices = np.concatenate([poses, last_row], axis = 1)
+
+    plot_scene(c2w_matrices, plot_eye_frustums = True, plot_cam_axes = False)
+
---
+++ nerf/utils/pose_utils.py
@@ -0,0 +1,114 @@
+import numpy as np
+
+def make_4x4(RT):
+    """
+    Converts a 3x4 RT matrix into a 4x4 RT matrix. The last 
+    row of the 4x4 matrix will be [0, 0, 0, 1].
+
+    TODO: Args, Returns?
+    """
+    assert RT.shape == (3, 4)
+
+    # The last row of np.eye(4) is [0, 0, 0, 1]. Hence, we 
+    # create np.eye(4) and overwrite the first three rows of 
+    # it with the RT matrix to get our desired output.
+    output = np.eye(4)
+    output[:3, :] = RT
+
+    return output
+
+def make_homogeneous(points):
+    """
+    Converts a points array of shape (N, 3) to (N, 4). The 
+    last column will have ones.
+
+    TODO: Args, Returns?
+    """
+    assert points.shape[1] == 3
+
+    ones = np.ones((points.shape[0], 1), dtype = np.float64)
+    output = np.concatenate([points, ones], axis = 1)
+
+    return output
+
+def transform_points(RT, points):
+    """
+    Applies the transformation to the given points.
+
+    TODO: Elaborate.
+    """
+    assert (RT.shape == (4, 4)) or (RT.shape == (3, 4)), (
+        "Shape of the RT matrix is invalid. Must be "
+        "either (3, 4) or (4, 4)"
+    )
+    assert points.shape[1] == 3
+
+    if RT.shape == (3, 4):
+        transform = make_4x4(RT)
+    elif RT.shape == (4, 4):
+        transform = RT
+
+    homogeneous_pts = make_homogeneous(points)
+    output = (transform @ homogeneous_pts.T).T
+
+    # Removing the last column which will be all ones.
+    output = output[:, :3]
+
+    return output
+
+def batched_transform_points(RT_matrices, points):
+    """
+    Applies all the RT_matrices on the given points.
+
+    TODO: Support (M, 3, 4) for RT_matrices also.
+    """
+    assert RT_matrices.shape[1:] == (4, 4), (
+        "Shape of the RT matrix is invalid. Must be (M, 4, 4)"
+    )
+    assert points.shape[1] == 3
+
+    homogeneous_pts = make_homogeneous(points)
+    output = (RT_matrices @ homogeneous_pts.T).transpose(0, 2, 1)
+
+    # Removing the last column which will be all ones.
+    output = output[..., :3]
+
+    return output
+
+def transform_line_segments(RT, lines):
+    """
+    Transforms line segments.
+    
+    Args:
+        RT      :   TODO, Explain.
+        lines   :   NumPy array of shape (N, 2, 3).
+
+    TODO: Elaborate
+    """
+    assert (lines.shape[1] == 2) and (lines.shape[2] == 3)
+    N = lines.shape[0]
+    points = lines.reshape(-1, 3)
+
+    out_points = transform_points(RT, points)
+    output = out_points.reshape(N, 2, 3)
+
+    return output
+
+def batched_transform_line_segments(RT_matrices, lines):
+    """
+    Transforms line segments.
+    
+    Args:
+        RT_matrices :   TODO, Explain.
+        lines       :   NumPy array of shape (N, 2, 3).
+
+    TODO: Elaborate
+    """
+    assert (lines.shape[1] == 2) and (lines.shape[2] == 3)
+    M, N = RT_matrices.shape[0], lines.shape[0]
+    points = lines.reshape(-1, 3)
+
+    out_points = batched_transform_points(RT_matrices, points)
+    output = out_points.reshape(M, N, 2, 3)
+
+    return output

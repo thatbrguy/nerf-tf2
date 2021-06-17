6968833d4a4c98dfef59f8c12099f3d37be72bbb
thatbrguy <bharathrajn98@gmail.com>
2021-06-17 23:49:21 +0530

    some changes and updates to ray utils

--- nerf/utils/ray_utils.py
+++ nerf/utils/ray_utils.py
@@ -7,32 +7,9 @@ import numpy as np
 
 from nerf.utils import pose_utils
 
-def get_rays_tf(H, W, focal, c2w):
+def get_rays():
     """
-    Gets ray origin and ray directions, TensorFlow.
-
-    TODO: Elaborate.
-    """
-    W_vals = tf.range(W, dtype = tf.float32)
-    H_vals = tf.range(H, dtype = tf.float32)
-
-    x, y = tf.meshgrid(W_vals, H_vals, indexing = "xy")
-
-    x_vals = x - (W / 2)
-    y_vals = y - (H / 2)
-    z_vals = tf.fill(x_vals.shape, focal, dtype = tf.float32)
-
-    directions = tf.stack([x_vals, y_vals, z_vals], axis = -1)
-
-    ## TODO: Separate rotation function for tensorflow?
-    rays_d = pose_utils.rotate_vectors(c2w, directions)
-    ## rays_o = tf.broadcast_to something! TODO: Finish!
-    
-    return rays_o, rays_d
-
-def get_rays_np():
-    """
-    Gets ray origin and ray directions, NumPy.
+    Gets ray origin and ray directions in the world coordinate system.
 
     TODO: Elaborate.
     """
@@ -41,16 +18,40 @@ def get_rays_np():
 
     x, y = np.meshgrid(W_vals, H_vals, indexing = "xy")
 
+    ## TODO: Write the more accurate version of this as well! 
+    ## Think of using cx, cy, fx, fy maybe?
     x_vals = x - (W / 2)
     y_vals = y - (H / 2)
     z_vals = np.full(x_vals.shape, focal, dtype = np.float64)
 
     directions = np.stack([x_vals, y_vals, z_vals], axis = -1)
 
+    ## TODO: Check output for correctness!
     rays_d = pose_utils.rotate_vectors(c2w, directions)
-    ## rays_o = np.broadcast_to something! TODO: Finish!
+    rays_o = np.broadcast_to(c2w[:, 3], rays_d.shape)
+
+    ## (TODO: Shape) --> (H*W, 3)
+    rays_d = rays_d.reshape(-1, 3)
+    rays_o = rays_o.reshape(-1, 3)
     
     return rays_o, rays_d
+
+def create_input_batch_coarse_model(params, rays_o, rays_d):
+    """
+    Given rays_o and rays_d, creates a batch of inputs that 
+    can be fed to the coarse model.
+
+    TODO: Elaborate!
+    """
+    pass
+
+def create_input_batch_fine_model(params, *args, **kwargs):
+    """
+    Creates batch of inputs for the fine model.
+
+    TODO: Elaborate!
+    """
+    pass
 
 if __name__ == '__main__':
 
